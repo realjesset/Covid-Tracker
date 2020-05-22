e1cd0a010af0abd050f5745447e251d9f80d1e43
Glider Gamez <thegoglider@gmail.com>
2020-05-22 19:18:26 +0400

    webpack config - sourcemap to false

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -30,10 +30,10 @@ const postcssNormalize = require("postcss-normalize");
 const appPackageJson = require(paths.appPackageJson);
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
-const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== "false";
+const shouldUseSourceMap = "false";
 // Some apps do not need the benefits of saving a web request, so not inlining the chunk
 // makes for a smoother build process.
-const shouldInlineRuntimeChunk = process.env.INLINE_RUNTIME_CHUNK !== "false";
+const shouldInlineRuntimeChunk = "false";
 
 const isExtendingEslintConfig = process.env.EXTEND_ESLINT === "true";
 
